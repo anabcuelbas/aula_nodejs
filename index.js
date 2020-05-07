988a00ffdcce59920c32f82339e4162a6631f47a
Ana Beatriz Cuelbas <abcuelbas@gmail.com>
2020-05-06 22:07:40 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo pelo Express'))
+
+app.listen(port, () => console.log('API rodando na porta 3000'))
